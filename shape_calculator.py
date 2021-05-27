78a4dd144ea247ac2d0cfd9acaf4ed8402cdf95c
jgeng98 <77642029+jgeng98@users.noreply.github.com>
2021-05-27 19:58:11 -0400

    add get_area and get_perimeter methods

--- shape_calculator.py
+++ shape_calculator.py
@@ -9,5 +9,10 @@ class Rectangle:
     def sef_height(self, new_height):
         self.height = new_height
 
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * self.width + 2 * self.height
 
 class Square:
