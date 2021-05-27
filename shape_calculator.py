d80e83b2ea6671601af8eed7a94d940ce95de0c6
jgeng98 <77642029+jgeng98@users.noreply.github.com>
2021-05-27 19:57:50 -0400

    add constructor and setter methods

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,13 @@
 class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
+    def set_width(self, new_width):
+        self.width = new_width
 
+    def sef_height(self, new_height):
+        self.height = new_height
 
 
 class Square:
